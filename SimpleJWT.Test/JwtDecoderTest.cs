dc5e481b3a2f1a4d6de773f60b19927b61ae5ed2
Claudio Bernasconi <email@claudio-bernasconi.net>
2017-04-04 08:45:01 +0200

    Added unit test to test for incorrect secret while decoding.

--- SimpleJWT.Test/JwtDecoderTest.cs
+++ SimpleJWT.Test/JwtDecoderTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SimpleJWT.Test
 {
@@ -6,7 +7,7 @@ namespace SimpleJWT.Test
 	public class JwtDecoderTest
 	{
 		[TestMethod]
-		public void Test()
+		public void Decode_with_correct_key()
 		{
 			var jwtDecoder = new JwtDecoder(new NewtonsoftJsonSerializer());
 
@@ -18,6 +19,18 @@ namespace SimpleJWT.Test
 			Assert.AreEqual("John Doe", payload["name"]);
 			Assert.AreEqual("1234567890", payload["sub"]);
 			Assert.AreEqual(true, payload["admin"]);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(Exception))]
+		public void Decode_with_incorrect_key()
+		{
+			var jwtDecoder = new JwtDecoder(new NewtonsoftJsonSerializer());
+
+			const string secret = "wrong-key";
+			const string jwt = "eyJUeXAiOiJKV1QiLCJBbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.du+/ve+/vUNj77+977+9Tu+/vdSmUzjvv71UBnFMbe+/vQTvv71b77+9yZ7vv73vv73vv70Y77+9Uw==";
+
+			jwtDecoder.Decode(jwt, secret);
 		}
 	}
 }
