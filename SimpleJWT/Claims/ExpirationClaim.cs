8c477c6529bc891ba87b07b1fb3c6fba038aa673
Claudio Bernasconi <email@claudio-bernasconi.net>
2017-04-10 10:49:06 +0200

    JWT Tokens expire after 30 minutes by default

--- SimpleJWT/Claims/ExpirationClaim.cs
+++ SimpleJWT/Claims/ExpirationClaim.cs
@@ -2,16 +2,16 @@
 
 namespace SimpleJWT.Claims
 {
-    internal class ExpirationClaim : IStandardClaim
-    {
-        public string Key
-        {
-            get { return "exp"; }
-        }
+	internal class ExpirationClaim : IStandardClaim
+	{
+		public string Key
+		{
+			get { return "exp"; }
+		}
 
-        public object GetValue()
-        {
-            return DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-        }
-    }
+		public object GetValue()
+		{
+			return DateTime.UtcNow.AddMinutes(30).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+		}
+	}
 }
