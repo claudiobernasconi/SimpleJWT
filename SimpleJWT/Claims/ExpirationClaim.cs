a4ac8db52faa53cb043ba8bd9dea7c35a7a0ca7f
Claudio Bernasconi <email@claudio-bernasconi.net>
2022-05-04 00:36:22 +0200

    ExpirationClaim: Add constructor parameter for expiration duration.

--- SimpleJWT/Claims/ExpirationClaim.cs
+++ SimpleJWT/Claims/ExpirationClaim.cs
@@ -2,16 +2,23 @@
 
 namespace SimpleJWT.Claims
 {
-	internal class ExpirationClaim : IStandardClaim
-	{
-		public string Key
-		{
-			get { return "exp"; }
-		}
+    internal class ExpirationClaim : IStandardClaim
+    {
+        private readonly int expirationInMinutes;
 
-		public object GetValue()
-		{
-			return DateTime.UtcNow.AddMinutes(30).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
-		}
-	}
+        public ExpirationClaim(int expirationInMinutes = 30)
+        {
+            this.expirationInMinutes = expirationInMinutes;
+        }
+
+        public string Key
+        {
+            get { return "exp"; }
+        }
+
+        public object GetValue()
+        {
+            return DateTime.UtcNow.AddMinutes(expirationInMinutes).Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+    }
 }
